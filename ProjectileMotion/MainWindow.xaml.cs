f4f2530eacf55cd6c9b2984e901d45cea98b0a56
Patryk Osiński <25852460+isoyost@users.noreply.github.com>
2019-12-12 10:08:21 +0100

    Version 1.0

---
+++ ProjectileMotion/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ProjectileMotion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawButton_OnClick(object sender, RoutedEventArgs routedEventArgs) => ProcessInput();
+
+        private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs) =>  ProcessInput(false);
+
+        private void ProcessInput(bool withExceptionDisplay = true)
+        {
+            double initialSpeed;
+            double angle;
+            double sampling;
+
+            try
+            {
+                initialSpeed = double.Parse(InitialSpeedBox.Text);
+                angle = double.Parse(AngleBox.Text);
+                sampling = double.Parse(SamplingBox.Text);
+            }
+            catch (FormatException)
+            {
+                if (withExceptionDisplay) MessageBox.Show("Input data must be a number.");
+                return;
+            }
+
+            if (ValidateValues(angle, initialSpeed, sampling))
+            {
+                DrawPolyline(angle, initialSpeed, sampling);
+            }
+        }
+
+        private bool ValidateValues(double angle, double speed, double sampling)
+        {
+            bool areValuesValid = true;
+            
+            if (speed < 0)
+            {
+                InitialSpeedHintTextBlock.Foreground = Brushes.Red;
+                areValuesValid = false;
+            }
+            else InitialSpeedHintTextBlock.Foreground = Brushes.Gray;
+            
+            if (angle > 90 || angle < 0)
+            {
+                AngleHintTextBlock.Foreground = Brushes.Red;
+                areValuesValid = false;
+            }
+            else AngleHintTextBlock.Foreground = Brushes.Gray;
+            
+            if (sampling < 0)
+            {
+                SamplingHintTextBlock.Foreground = Brushes.Red;
+                areValuesValid = false;
+            }
+            else SamplingHintTextBlock.Foreground = Brushes.Gray;
+
+            return areValuesValid;
+        }
+
+        private void DrawPolyline(double angle, double speed, double sampling)
+        {
+            ProjectileMotionCalculator calculator = new ProjectileMotionCalculator(angle, speed, sampling);
+            PointCollection motion = calculator.CountMotion();
+            double height = Border.ActualHeight;
+            ProjectileMotionPolyline.Points = AdjustToWindow(motion, height);
+        }
+
+        private static PointCollection AdjustToWindow(PointCollection pointCollection, double height)
+        {
+            PointCollection adjustedPointCollection = new PointCollection();
+
+            foreach (Point point in pointCollection)
+            {
+                var y = point.Y + height - 2;
+                var x = point.X;
+                adjustedPointCollection.Add(new Point(x, y));
+            }
+            
+            return adjustedPointCollection;
+        }
+    }
+}
---
+++ ProjectileMotion/ProjectileMotionCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ProjectileMotion
+{
+    public class ProjectileMotionCalculator
+    {
+        private readonly double _angleSin;
+        private readonly double _angleCos;
+        private readonly double _speed;
+        private readonly double _sampling;
+        private const double Gravity = 9.81;
+
+        public ProjectileMotionCalculator(double angle, double initialSpeed, double sampling)
+        {
+            if (angle > 90 || angle < 0) throw new Exception("Invalid angle value.");
+            if (initialSpeed > 1000 || initialSpeed < 0) throw new Exception("Invalid speed value.");
+            if (sampling <= 0) throw new Exception("Sampling equal or lesser than 0.");
+            angle = ToRadians(angle);
+            _angleSin = Math.Sin(angle);
+            _angleCos = Math.Cos(angle);
+            _speed = initialSpeed;
+            _sampling = 1 / sampling;
+        }
+
+        public PointCollection CountMotion()
+        {
+            PointCollection motion = new PointCollection();
+            double currentX;
+            double currentY;
+            double time = 0;
+
+            do
+            {
+                currentX = CalculateX(time);
+                // multiplication by -1 because otherwise graph would be upside down
+                currentY = -1 * CalculateY(time);
+                motion.Add(new Point(currentX, currentY));
+                time += _sampling;
+            } while (currentY <= 0);
+
+            return motion;
+        }
+
+        private double CalculateX(double time) => _speed * _angleCos * time;
+
+        private double CalculateY(double time) => _speed * _angleSin * time - 0.5 * Gravity * Math.Pow(time, 2);
+
+        private static double ToRadians(double angle) => angle * Math.PI / 180;
+    }
+}
